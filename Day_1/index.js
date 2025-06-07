587cb1195be5b53d077fbd7c1ace1b1930df999b
sunny <kushsunny2002@gmail.com>
2025-06-07 16:53:29 +0530

    calculator project

---
+++ Day_1/index.js
@@ -0,0 +1,13 @@
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.clear();
+// alert('welcome to JavaScript');
